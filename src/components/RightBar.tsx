cc0480a7a6e31c1b86bbe4687af2a0fcd17d241d
Fazliddin <askaraliev14@mail.ru>
2024-02-28 19:59:21 +0500

    Responsive

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -22,6 +22,7 @@ function RightBar() {
     const dispatch = useDispatch();
     const [searchTerm, setSearchTerm] = useState('')
     const [city, setCity] = useState('Tashkent')
+    const [navBar, setNavBar] = useState(false);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
     const weather = useSelector((state: { weather: WeatherData }) => state.weather.data);
 
@@ -41,67 +42,77 @@ function RightBar() {
             dispatch(setWeatherData(weatherData))
         }
     }, [isLoading, error, weatherData, dispatch])
-
     const handleSearch = () => {
         setCity(searchTerm)
+        setNavBar(false)
+    }
+    const searchList = (value) => {
+        setCity(value)
+        setNavBar(false)
+    }
+    function handleClick() {
+        setNavBar(!navBar);
     }
     return (
-        <div className="right-container">
-            <div className="search-box">
-                <input
-                    className="search-box__input"
-                    placeholder="Another location"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
-                />
-                <button className="search-box__btn" onClick={handleSearch}/>
-            </div>
-            <p className={`error-message ${errorMsg ? '' : 'hidden'}`}>{errorMsg}</p>
-            <div className="scroll_box">
-                <ul>
-                    {locations.map(e => (
-                        <li
-                            className="font-secondary"
-                            key={e.id}
-                            onClick={() => setCity(e.value)}
-                        >
-                            {e.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <hr className="divider"/>
-            <div className="weather-detail">
-                <h2 className="font-primary">Weather Detail</h2>
-                <div className="weather-detail__list">
-                    <div className="weather-detail__item">
-                        <p className="font-secondary">
-                            Cloudy
-                        </p>
-                        <p className="font-primary">{weather?.clouds?.all || ''}%</p>
-                    </div>
-                    <div className="weather-detail__item">
-                        <p className="font-secondary">
-                            Humidity
-                        </p>
-                        <p className="font-primary">{weather?.main?.humidity || ''}%</p>
-                    </div>
-                    <div className="weather-detail__item">
-                        <p className="font-secondary">
-                            Wind
-                        </p>
-                        <p className="font-primary">{weather?.wind?.speed || ''}km/h</p>
-                    </div>
-                    <div className="weather-detail__item">
-                        <p className="font-secondary">
-                            Pressure
-                        </p>
-                        <p className="font-primary">{weather?.main?.pressure || ''}km/h</p>
+        <>
+            <div className={`${navBar ? 'close-icon ' : 'burger-menu'}`} onClick={handleClick}/>
+            <div className={`right-container ${navBar ? 'display-navbar scale-up-hor-right' : ''}`}>
+                <div className="search-box">
+                    <input
+                        className="search-box__input"
+                        placeholder="Another location"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+                    />
+                    <button className="search-box__btn" onClick={handleSearch}/>
+                </div>
+                <p className={`error-message ${errorMsg ? '' : 'hidden'}`}>{errorMsg}</p>
+                <div className="scroll_box">
+                    <ul>
+                        {locations.map(e => (
+                            <li
+                                className="font-secondary"
+                                key={e.id}
+                                onClick={() => searchList(e.value)}
+                            >
+                                {e.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <hr className="divider"/>
+                <div className="weather-detail">
+                    <h2 className="font-primary">Weather Detail</h2>
+                    <div className="weather-detail__list">
+                        <div className="weather-detail__item">
+                            <p className="font-secondary">
+                                Cloudy
+                            </p>
+                            <p className="font-primary">{weather?.clouds?.all || ''}%</p>
+                        </div>
+                        <div className="weather-detail__item">
+                            <p className="font-secondary">
+                                Humidity
+                            </p>
+                            <p className="font-primary">{weather?.main?.humidity || ''}%</p>
+                        </div>
+                        <div className="weather-detail__item">
+                            <p className="font-secondary">
+                                Wind
+                            </p>
+                            <p className="font-primary">{weather?.wind?.speed || ''}km/h</p>
+                        </div>
+                        <div className="weather-detail__item">
+                            <p className="font-secondary">
+                                Pressure
+                            </p>
+                            <p className="font-primary">{weather?.main?.pressure || ''}km/h</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
